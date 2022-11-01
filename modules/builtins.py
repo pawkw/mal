100ed25dacba85f75ff14c9b671e04bd4842b972
pawkw <pawkw@users.noreply.github.com>
2022-11-01 03:42:44 -0400

    Added comparisons to step 4.

--- modules/builtins.py
+++ modules/builtins.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import operator
 from modules.MalType import MalType
 from modules.print_mal import pr_str
 from typing import List
@@ -23,6 +24,28 @@ def mal_println(args):
     print(mal_str(args, "").data)
     return MalType.nil()
 
+def mal_compare(args, op):
+    result = MalType.true() if op(args[0].data, args[1].data) else MalType.false()
+    if len(args) < 3:
+        return result
+    return MalType.true() if result.isType('true') and mal_compare(args[1:], op).isType('true') else MalType.false()
+
+def mal_equate(args):
+    if len(args) < 3:
+        if args[0].type == args[1].type:
+            if not args[0].type in ['list', 'vector', 'hashmap']:
+                return MalType.true() if args[0].data == args[1].data else MalType.false()
+            if len(args[0].data) != len(args[1].data):
+                return MalType.false()
+            for x, y in zip(args[0].data, args[1].data):
+                if mal_equate([x, y]).isType('false'):
+                    return MalType.false()
+            return MalType.true()
+        return MalType.false()
+    rest = mal_equate(args[1:])
+    result = mal_equate([args[0], args[1]])
+    return MalType.true() if rest.isType('true') and result.isType('true') else MalType.false()
+
 builtins = {
     '+': MalType.function(lambda args, _: reduce(lambda x, y: MalType.integer(x.data + y.data), args), builtin=True),
     '-': MalType.function(lambda args, _: reduce(lambda x, y: MalType.integer(x.data - y.data), args), builtin=True),
@@ -35,8 +58,17 @@ builtins = {
     'if': MalType.symbol('if'),
     'fn*': MalType.symbol('fn*'),
     'env': MalType.symbol('env'),
-    'prn': MalType.function(lambda args, _: mal_prn(args)),
-    'pr-str': MalType.function(lambda args, _: mal_pr_str(args)),
-    'str': MalType.function(lambda args, _: mal_str(args, " ")),
-    'println': MalType.function(lambda args, _: mal_println(args))
+    'prn': MalType.function(lambda args, _: mal_prn(args), builtin=True),
+    'pr-str': MalType.function(lambda args, _: mal_pr_str(args), builtin=True),
+    'str': MalType.function(lambda args, _: mal_str(args, " "), builtin=True),
+    'println': MalType.function(lambda args, _: mal_println(args), builtin=True),
+    'list': MalType.function(lambda args, _: MalType.list(args), builtin=True),
+    'list?': MalType.function(lambda args, _: MalType.true() if args[0].isType('list') else MalType.false(), builtin=True),
+    'empty?': MalType.function(lambda args, _: MalType.true() if len(args[0].data) < 1 else MalType.false(), builtin=True),
+    'count': MalType.function(lambda args, _: MalType.integer(len(args[0].data)), builtin=True),
+    '<': MalType.function(lambda args, _: mal_compare(args, operator.lt), builtin=True),
+    '<=': MalType.function(lambda args, _: mal_compare(args, operator.le), builtin=True),
+    '>': MalType.function(lambda args, _: mal_compare(args, operator.gt), builtin=True),
+    '>=': MalType.function(lambda args, _: mal_compare(args, operator.ge), builtin=True),
+    '=': MalType.function(lambda args, _: mal_equate(args), builtin=True)
 }
